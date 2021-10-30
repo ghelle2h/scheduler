5e3a6b815a2189d02c102f73db67c8b2c96858aa
Hamza Ghelle <hamzaghellegmail.com>
2021-10-30 16:53:47 -0400

    add logic to scheduler app

---
+++ src/components/DayListItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
+
+
+export default function DayListItem(props) {
+  let dayClass = classNames("day-list__item", {
+    "day-list__item--selected" : props.selected,
+    "day-list__item--full" : props.spots === 0
+  })
+  function formatSpots(props) {
+    return props.spots === 0 ? "no spots remaining"
+         : (props.spots === 1 ? "1 spot remaining"
+         : `${props.spots} spots remaining`)
+  }
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)} selected={props.selected}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{formatSpots}</h3>
+    </li>
+  );
+}
---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "components/InterviewerListItem.scss"
+import classNames from "classnames";
+
+export default function InterviewerListItem(props) {
+  const selectedInterviewer = classNames("interviewers__item",{
+    "interviewers__item--selected" : props.selected
+  });
+
+  return (
+    <li className={selectedInterviewer} selected={props.selected} onClick={props.setInterviewer} >
+      <img 
+      className="interviewers__item-image"
+      src={props.avatar}
+      alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  )
+}
